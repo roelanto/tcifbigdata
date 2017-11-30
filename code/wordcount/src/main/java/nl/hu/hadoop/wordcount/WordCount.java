bc129f875e55e9e297b57d2f908ecd69e6228568
Roelant Ossewaarde <roelanto@gmail.com>
2017-11-30 23:00:01 +0100

    Path can now be randomized. Added input-dir.

--- code/wordcount/src/main/java/nl/hu/hadoop/wordcount/WordCount.java
+++ code/wordcount/src/main/java/nl/hu/hadoop/wordcount/WordCount.java
@@ -1,22 +1,40 @@
 package nl.hu.hadoop.wordcount;
 
-import java.io.IOException;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.*;
-import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
 
 public class WordCount {
 
 	public static void main(String[] args) throws Exception {
 		Job job = new Job();
 		job.setJarByClass(WordCount.class);
-
+		/*
+		 * Volgende twee regels bepalen waar input gelezen wordt: input van cmd-line argument #1, output naar cmd-line argument #2.
+		 * Bijvoorbeeld: specificeer op de command-line "myinput" "myoutput" en de files in "myinput" worden gelezen en output
+		 * wordt weggeschreven naar "myoutput".
+		 * Let op: Hadoop geeft een error als je probeert naar een bestaande output-directory te schrijven, dus maak die
+		 * of uniek voor elke keer (evt. programmatisch) of verwijder de output-directory iedere keer.
+		 */
 		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		String pathname = args[1];
+		/*
+		 * uncomment the following code to append a random number (0-9999) to the output dir each time.
+		 */
+		// pathname = ""+args[1]+"."+ new Random().nextInt(9999);
+		// System.err.println("Output was sent to directory "+pathname);
+
+		FileOutputFormat.setOutputPath(job, new Path(pathname));
 
 		job.setMapperClass(WordCountMapper.class);
 		job.setReducerClass(WordCountReducer.class);
